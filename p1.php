d91279641bfa0b7dc67b187300abd554cc90df16
KARRI-LOKESH <142475625+KARRI-LOKESH@users.noreply.github.com>
2024-04-12 15:57:11 +0530

    Update p1.php

--- p1.php
+++ p1.php
@@ -56,6 +56,21 @@ myFunc();
 echo "<br>";
 myFunc();
 echo "<br>";
+echo "<br>";
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+  public function message() {
+    return "My car is a " . $this->color . " " . $this->model . "!";
+  }
+}
+
+$myCar = new Car("red", "Volvo");
+var_dump($myCar);
 ?>
 </body>
 </html>

c6734a638c04cf0e31bb099bd92e2fb8a8cc9c9d
KARRI-LOKESH <142475625+KARRI-LOKESH@users.noreply.github.com>
2024-04-12 15:46:22 +0530

    Update p1.php

--- p1.php
+++ p1.php
@@ -1,10 +1,63 @@
 <!DOCTYPE html>
 <html>
-    <body>
-        <?php
-        $gana=$_POST['acno'];
-        echo $gana;
-        ?>
-        </body>
-        </html>
+<body>
+<?php
+$color="red";
+/*hszgjhxvc bhsdfhgvcghdfcvbncvbncd*/
+//ujsyaguchvhbsadhcvghsxv ghazcv//
+#dhsfghcvsdghcvhndfgvbhvfhgh/
+
+echo "lokesh is a $color boy"."<br>";
+echo "lokesh is a $color boy"."<br>";
+echo "lokesh is a $color boy"."<br>";
+
+$x=5 /*jxhvdgvhj*/ +5;
+echo "<h1> $x is a lokesh marks </h1>"."<br>";
+echo "hello world";
+echo "<br>";
+/*       */
+$y="teja";
+echo "lokesh is $y's friend"."<br>";
+var_dump(5);
+echo "<br>";
+var_dump("john");
+echo "<br>";
+var_dump(7.89);
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump([1,2,4]);
+$s=$z=$a=10;
+echo "<br>";
+echo $s."<br>";
+echo $z."<br>";
+echo $a."<br>";
+echo "<p> variable x inside function is:$x </p>";
+function myTest(){
+$t=17;
+echo "<p> variable x inside function is:$t </p>";}
+myTest();
+echo "<br>";
+$e=14;
+$g=20;
+function myLok(){
+$GLOBALS["Y"]=$GLOBALS["e"]+$GLOBALS["g"];
+}
+myLok();
+echo $Y;
+echo "<br>";
+function myFunc(){
+static $xy=0;
+echo $xy;
+$xy++;}
+myFunc();
+echo "<br>";
+myFunc();
+echo "<br>";
+myFunc();
+echo "<br>";
+?>
+</body>
+</html>
+
         
